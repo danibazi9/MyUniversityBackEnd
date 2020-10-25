e236691e33a5379c40f78960c8b75712861b1812
Danial <danibazi9@gmail.com>
2020-10-26 01:41:47 +0330

    Add class for User Serialization response

--- MyUniversity/serializers.py
+++ MyUniversity/serializers.py
@@ -5,4 +5,4 @@ from MyUniversity.models import *
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email')
+        fields = ('first_name', 'last_name', 'email', 'student_id')
