74cd9a328a4f995e10efaff19d6b6e1c502e8de7
Danial <danibazi9@gmail.com>
2020-10-25 20:00:11 +0330

    Serialize file created for API response

---
+++ MyUniversity/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from MyUniversity.models import *
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
