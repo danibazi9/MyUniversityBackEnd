e83fa7d1dfe46df85dc798fc3935fb73dcb19d8b
Danial <danibazi9@gmail.com>
2020-10-26 01:39:29 +0330

    Change the user & password to the organization email

--- MyUniversityBackEnd/settings.py
+++ MyUniversityBackEnd/settings.py
@@ -107,8 +107,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'danibazi9@gmail.com'
-EMAIL_HOST_PASSWORD = 'danial141278'
+EMAIL_HOST_USER = 'info.myuniversity@gmail.com'
+EMAIL_HOST_PASSWORD = 'develover1399'
 
 
 # Internationalization
