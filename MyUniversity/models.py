ef466befa78e31d53fc47ec25382f38634b69fdd
Danial <danibazi9@gmail.com>
2020-10-25 20:01:13 +0330

    Some sample changes in urls & models to run the API

--- MyUniversity/models.py
+++ MyUniversity/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=20)
+    student_id = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
--- MyUniversity/views.py
+++ MyUniversity/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from rest_framework import status
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from . models import User
+from . serializers import UserSerializer
+
 
 # Create your views here.
+class UsersList(APIView):
+    def get(self, request):
+        user = User.objects.all()
+        serializer = UserSerializer(user, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
