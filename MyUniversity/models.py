4caf63f1d58946661e6147828d3c06f57d835c29
Danial <danibazi9@gmail.com>
2020-10-26 13:18:28 +0330

    Add primary key & set the student id unique

--- MyUniversity/models.py
+++ MyUniversity/models.py
@@ -3,19 +3,20 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     email = models.EmailField(max_length=256)
-    student_id = models.IntegerField(default=0)
+    student_id = models.IntegerField(unique=True)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
 
 
-class EmailVerify(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    vc_code = models.IntegerField(default=0)
-    expired_time = models.TimeField()
-
-    def __str__(self):
-        return self.user.email + " " + str(self.vc_code)
+# class EmailVerify(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     vc_code = models.IntegerField(default=0)
+#     expired_time = models.TimeField()
+#
+#     def __str__(self):
+#         return self.user.email + " " + str(self.vc_code)
