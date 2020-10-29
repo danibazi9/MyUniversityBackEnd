ee9eac4c5d71aaad15ac1567e0fe8d7dfb0b53e8
Danial <danibazi9@gmail.com>
2020-10-29 07:38:29 +0330

    Define UserSerializer class for rest_framework

--- MyUniversity/serializers.py
+++ MyUniversity/serializers.py
@@ -3,6 +3,10 @@ from MyUniversity.models import *
 
 
 class UserSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(required=False)
+    last_name = serializers.CharField(required=False)
+    email = serializers.EmailField(required=False)
+
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email', 'student_id')
