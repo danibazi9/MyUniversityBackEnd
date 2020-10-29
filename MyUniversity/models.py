2ebbef57d7f819fda218e8d02eac952e47f19495
Danial <danibazi9@gmail.com>
2020-10-29 11:57:16 +0330

    The mobile_number field type changed to CharField in DB

--- MyUniversity/models.py
+++ MyUniversity/models.py
@@ -8,7 +8,7 @@ class User(models.Model):
     last_name = models.CharField(max_length=40)
     email = models.EmailField(max_length=256, unique=True)
     student_id = models.IntegerField(unique=True)
-    mobile_number = models.BigIntegerField(default=9100000000)
+    mobile_number = models.CharField(max_length=11, default="09100000000")
     password = models.CharField(max_length=20, validators=[MinLengthValidator(6)], blank=True)
 
     def __str__(self):
