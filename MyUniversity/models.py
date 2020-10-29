1d3a7f8b9602d217c7452e5f42f96be91eb696b1
Danial <danibazi9@gmail.com>
2020-10-29 07:04:31 +0330

    The database has modified

--- MyUniversity/models.py
+++ MyUniversity/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 class University(models.Model):
-    university_id = models.IntegerField(primary_key=True)
+    university_id = models.AutoField(primary_key=True)
     university_name = models.CharField(max_length=30, unique=True)
     university_domain = models.CharField(max_length=10, unique=True)
 
@@ -19,7 +19,7 @@ class User(models.Model):
     email = models.EmailField(max_length=256, unique=True)
     student_id = models.IntegerField()
     university = models.ForeignKey(University, on_delete=models.CASCADE)
-    mobile_number = models.BigIntegerField()
+    mobile_number = models.BigIntegerField(default=9100000000)
     password = models.CharField(max_length=20, validators=[MinLengthValidator(6)], blank=True)
 
     def __str__(self):
