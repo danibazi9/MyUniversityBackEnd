39c4895d4cd39caf69d34c72e5d7a5787956e176
Danial <danibazi9@gmail.com>
2020-11-07 16:00:05 +0330

    Write the db structure of chat

--- MyUniversity/models.py
+++ MyUniversity/models.py
@@ -13,3 +13,31 @@ class User(models.Model):
 
     def __str__(self):
         return self.first_name + " " + self.last_name + " " + str(self.student_id)
+
+
+class Contact(models.Model):
+    user = models.ForeignKey(User, related_name='friends', on_delete=models.CASCADE)
+    friends =models.ManyToManyField('self', blank=True)
+
+    def __str__(self):
+        return self.user.last_name + " " + str(self.user.student_id)
+
+
+class Message(models.Model):
+    contact = models.ForeignKey(Contact, related_name='messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.contact.user.first_name + " " + self.contact.user.last_name
+
+
+class Chat(models.Model):
+    participants = models.ManyToManyField(Contact, related_name='chats')
+    messages = models.ManyToManyField(Message, blank=True)
+
+    def last_10_messages(self):
+        return self.messages.objects.order_by('-timestamp').all()[::-1]
+
+    def __str__(self):
+        return "{}".format(self.pk)
