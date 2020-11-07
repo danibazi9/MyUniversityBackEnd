50df303a124ddd28ca83a851ea00d7744014e19f
unknown <github.edu@hotmail.com>
2020-11-07 14:53:45 +0330

    Book, Faculty & Field DB Model added.

--- MyUniversity/admin.py
+++ MyUniversity/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.User)
+admin.site.register(models.Field)
+admin.site.register(models.Faculty)
+admin.site.register(models.Book)
--- MyUniversity/models.py
+++ MyUniversity/models.py
@@ -13,3 +13,30 @@ class User(models.Model):
 
     def __str__(self):
         return self.first_name + " " + self.last_name + " " + str(self.student_id)
+
+class Faculty(models.Model):
+    faculty_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Field(models.Model):
+    field_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Faculty: " + self.faculty.name + ", Field: " + self.name
+
+class Book(models.Model):
+    book_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    author = models.CharField(max_length=50)
+    publisher = models.CharField(max_length=50)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    field = models.ForeignKey(Field, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Name: " + self.name + ", Author: " + self.author
+
