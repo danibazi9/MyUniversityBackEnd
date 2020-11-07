f2d3f8e9b8af5c989b23662e239950e1a7fa1396
Danial <danibazi9@gmail.com>
2020-11-07 21:25:21 +0330

    Set urls in url files of apps

--- chat/urls.py
+++ chat/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import api
 
-app_name = 'MyUniversity'
+app_name = 'chat'
 urlpatterns = [
     path('chat-list/', api.ChatList.as_view()),
     path('chat-list/<int:stuID>', api.ChatDetails.as_view()),
