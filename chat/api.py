34ffffe4ea99114a84896ae924ba1a80307d591b
Danial <danibazi9@gmail.com>
2020-11-11 23:42:41 +0330

    Create api for add a chatroom between two students in DB

--- chat/api.py
+++ chat/api.py
@@ -24,7 +24,44 @@ class ChatList(APIView):
         serializer = ChatSerializer(all_chats, many=True)
         return Response(serializer.data)
 
-    # def post(self, request):
+    def post(self, request):
+        chat_serializer = ChatSerializer(data=request.data)
+        if chat_serializer.is_valid():
+            sender_stuID = request.POST['sender_student_id']
+            receiver_stuID = request.POST['receiver_student_id']
+
+            sender_user = get_user(sender_stuID)
+            if not sender_user:
+                return Response(f"User with student_id {sender_stuID} Not Found!", status=status.HTTP_404_NOT_FOUND)
+
+            receiver_user = get_user(receiver_stuID)
+            if not receiver_user:
+                return Response(f"User with student_id {receiver_stuID} Not Found!", status=status.HTTP_404_NOT_FOUND)
+
+            try:
+                sender_contact = Contact.objects.get(user__student_id=sender_stuID)
+            except Contact.DoesNotExist:
+                sender_contact = Contact.objects.create(user=sender_user)
+                sender_contact.save()
+
+            try:
+                receiver_contact = Contact.objects.get(user__student_id=receiver_stuID)
+            except Contact.DoesNotExist:
+                receiver_contact = Contact.objects.create(user=receiver_user)
+                receiver_contact.save()
+
+            sender_contact.friends.add(receiver_contact)
+
+            try:
+                new_chat = Chat.objects.get(title=request.POST['title'])
+            except Chat.DoesNotExist:
+                new_chat = Chat.objects.create(title=request.POST['title'])
+            new_chat.user.add(sender_contact)
+            new_chat.user.add(receiver_contact)
+
+            return Response(f"Chatroom with id {new_chat.id} has successfully created "
+                            f"between {sender_user.student_id} & {receiver_user.student_id}!", status=status.HTTP_201_CREATED)
+        return Response(chat_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ChatDetails(APIView):
