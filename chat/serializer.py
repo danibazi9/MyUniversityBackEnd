61c67baa37b966b559935ef8be86e43abfc4351c
Danial <danibazi9@gmail.com>
2020-11-11 23:34:07 +0330

    Add Serializer class for Chat, Message & Contact class

--- chat/serializer.py
+++ chat/serializer.py
@@ -1,12 +1,32 @@
+from django.db.models.functions import datetime
 from rest_framework import serializers
+
+from MyUniversity.serializer import UserSerializer
+from chat.models import *
 from MyUniversity.models import *
 
 
+class ContactSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+    friends = serializers.StringRelatedField(read_only=True, many=True)
+
+    class Meta:
+        model = Contact
+        fields = ('user', 'friends')
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    contact = serializers.StringRelatedField(read_only=True)
+    timestamp = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+
+    class Meta:
+        model = Message
+        fields = ('contact', 'content', 'timestamp')
+
+
 class ChatSerializer(serializers.ModelSerializer):
-    first_name = serializers.CharField(required=False)
-    last_name = serializers.CharField(required=False)
-    email = serializers.EmailField(required=False)
+    messages = MessageSerializer(read_only=True, many=True)
 
     class Meta:
         model = Chat
-        fields = '__all__'
+        fields = ('title', 'messages')
