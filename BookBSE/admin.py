770a7aecf1a442aa454be9e11973a104f6405241
unknown <github.edu@hotmail.com>
2020-11-12 23:01:17 +0330

    create Field model & apply some changes & update admin.py to recognize it

--- BookBSE/admin.py
+++ BookBSE/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from BookBSE import models
 # Register your models here.
 admin.site.register(models.Faculty)
+admin.site.register(models.Field)
--- BookBSE/models.py
+++ BookBSE/models.py
@@ -6,3 +6,10 @@ class Faculty(models.Model):
 
     def __str__(self):
         return self.name
+
+class Field(models.Model):
+    name = models.CharField(max_length=125, unique=True)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Faculty: {self.faculty.name}; Field: {self.name}"
