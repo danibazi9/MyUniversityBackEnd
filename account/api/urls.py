39ece6813e4ec6f95754ef9577cccd8aac765c36
sina_ziaee <sinaZiaee99@gmail.com>
2020-11-13 03:31:57 +1030

    adding email_verification

--- account/api/urls.py
+++ account/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from account.api.views import registration_view
+from account.api.views import registration_view, update_account_view, account_properties_view
 from rest_framework.authtoken.views import obtain_auth_token
 
 app_name = 'account'
 
 urlpatterns = [
     path('register', registration_view, name='register'),
+    path('update', update_account_view, name='update'),
     path('login', obtain_auth_token, name='login'),
 ]
--- account/api/views.py
+++ account/api/views.py
@@ -1,9 +1,11 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
-from account.api.serializers import RegistrationSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from account.api.serializers import RegistrationSerializer, AccountPropertiesSerializer
 from rest_framework.authtoken.models import Token
+from validate_email import validate_email
+from account.models import Account
 
 
 @api_view(['POST'])
@@ -11,10 +13,52 @@ def registration_view(request):
     serializer = RegistrationSerializer(data=request.data)
     data = {}
     if serializer.is_valid():
-        account = serializer.save()
-        data['response'] = 'successful'
-        token = Token.objects.get(user=account).key
-        data['token'] = token
+        email = request.data['email']
+        if validate_email(serializer.validated_data.get('email')):
+            account = serializer.save()
+            data['response'] = 'successful'
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+            return Response(data=data, status=status.HTTP_201_CREATED)
+            # if serializer.validated_data.get('email').endswith('.iust.ac.ir'):
+            #     serializer.save()
+            #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+            # else:
+            #     return Response(f"The email '{serializer['email'].value}' isn't the academical university email",
+            #                     status=status.HTTP_406_NOT_ACCEPTABLE)
+        else:
+            return Response(f"The email '{serializer['email'].value}' doesn't exist",
+                            status=status.HTTP_406_NOT_ACCEPTABLE)
     else:
-        data['response'] = 'failure'
-    return Response(data=data)
+        return Response(f"The email '{serializer['email'].value}' doesn't exist",
+                        status=status.HTTP_406_NOT_ACCEPTABLE)
+
+
+@api_view(['GET', ])
+def account_properties_view(request):
+    try:
+        account = request.user
+    except Account.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = AccountPropertiesSerializer(account)
+        return Response(serializer.data)
+
+
+@api_view(['PUT', ])
+@permission_classes((IsAuthenticated,))
+def update_account_view(request):
+    try:
+        account = request.user
+    except Account.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = AccountPropertiesSerializer(account, data=request.data)
+        data = {}
+        if serializer.is_valid():
+            serializer.save()
+            data['response'] = 'Account update success'
+            return Response(data=data, status=status.HTTP_205_RESET_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
