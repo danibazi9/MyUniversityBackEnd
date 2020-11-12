d14e592bcd96cc7cb18e683c2a0999cab9971e79
unknown <github.edu@hotmail.com>
2020-11-12 22:54:20 +0330

    create Faculty model & apply some changes & update admin.py to recognize it

--- BookBSE/models.py
+++ BookBSE/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Faculty(models.Model):
+    name = models.CharField(max_length=125, unique=True)
+
+    def __str__(self):
+        return self.name
