20ce5a95c2d8d02c2f3f4c7913010cafa2153d64
sina_ziaee <sinaZiaee99@gmail.com>
2020-11-13 04:38:21 +1030

    adding chat with database handling

--- chat/urls.py
+++ chat/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path, re_path
+from django.urls import path
 
-from . import api
-
+from . import views
 app_name = 'chat'
+
 urlpatterns = [
-    path('chat-room-list/', api.ChatList.as_view()),
-    path('chat-room-list/<int:stuID>', api.ChatDetails.as_view()),
-    path('chat-contact-list/', api.ContactList.as_view()),
-    # path('chat-contacts-list/<int:stuID>', api.ContactDetails.as_view()),
+    path('', views.index, name='index'),
+    path('<str:room_name>/', views.room, name='room'),
+
 ]
