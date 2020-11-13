fdbb308f7f5af70f5b418f569a2e8ad08fdba808
Danial <danibazi9@gmail.com>
2020-11-14 02:57:53 +0330

    Create API for see room details

--- chat/api.py
+++ chat/api.py
@@ -2,84 +2,94 @@ import json
 import random
 
 from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .models import *
 from .serializer import *
 
 
-def get_user(stuID):
+def get_user(request):
     try:
-        user_to_find = User.objects.get(student_id=stuID)
-        return user_to_find
-    except User.DoesNotExist:
-        return None
+        account = request.user
+    except Account.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        return account
 
 
 # Create your views here.
-class ChatList(APIView):
-    def get(self, request):
-        all_chats = Chat.objects.all()
-        serializer = ChatSerializer(all_chats, many=True)
-        return Response(serializer.data)
+@api_view(['GET', ])
+def chat_properties_view(request):
+    try:
+        account = request.user
+    except Account.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    def post(self, request):
-        chat_serializer = ChatSerializer(data=request.data)
-        if chat_serializer.is_valid():
-            sender_stuID = request.POST['sender_student_id']
-            receiver_stuID = request.POST['receiver_student_id']
+    if request.method == 'GET':
+        rooms = Room.objects.filter(first_user_id=account) | Room.objects.filter(second_user_id=account)
+        serializer = RoomSerializer(rooms, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-            sender_user = get_user(sender_stuID)
-            if not sender_user:
-                return Response(f"User with student_id {sender_stuID} Not Found!", status=status.HTTP_404_NOT_FOUND)
-
-            receiver_user = get_user(receiver_stuID)
-            if not receiver_user:
-                return Response(f"User with student_id {receiver_stuID} Not Found!", status=status.HTTP_404_NOT_FOUND)
-
-            try:
-                sender_contact = Contact.objects.get(user__student_id=sender_stuID)
-            except Contact.DoesNotExist:
-                sender_contact = Contact.objects.create(user=sender_user)
-                sender_contact.save()
-
-            try:
-                receiver_contact = Contact.objects.get(user__student_id=receiver_stuID)
-                if receiver_contact:
-                    return Response("This chatroom has already exists!", status=status.HTTP_302_FOUND)
-            except Contact.DoesNotExist:
-                receiver_contact = Contact.objects.create(user=receiver_user)
-                receiver_contact.save()
-
-            sender_contact.friends.add(receiver_contact)
-
-            try:
-                new_chat = Chat.objects.get(title=request.POST['title'])
-            except Chat.DoesNotExist:
-                new_chat = Chat.objects.create(title=request.POST['title'])
-            new_chat.user.add(sender_contact)
-            new_chat.user.add(receiver_contact)
-
-            return Response(f"Chatroom with id {new_chat.id} has successfully created "
-                            f"between {sender_user.student_id} & {receiver_user.student_id}!", status=status.HTTP_201_CREATED)
-        return Response(chat_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class ChatDetails(APIView):
-    def get(self, request, stuID):
-        user = get_user(stuID)
-        if not user:
-            return Response(f"User with student_id {stuID} Not Found!", status=status.HTTP_404_NOT_FOUND)
-
-        try:
-            contact = Contact.objects.get(user=user)
-        except Contact.DoesNotExist:
-            return Response(f"Contact with student_id {stuID} Not Found!", status=status.HTTP_404_NOT_FOUND)
-
-        chats = contact.chats.all()
-        serializer = ChatSerializer(chats, many=True)
-        return serializer.data
+#     def post(self, request):
+#         chat_serializer = ChatSerializer(data=request.data)
+#         if chat_serializer.is_valid():
+#             sender_stuID = request.POST['sender_student_id']
+#             receiver_stuID = request.POST['receiver_student_id']
+#
+#             sender_user = get_user(sender_stuID)
+#             if not sender_user:
+#                 return Response(f"User with student_id {sender_stuID} Not Found!", status=status.HTTP_404_NOT_FOUND)
+#
+#             receiver_user = get_user(receiver_stuID)
+#             if not receiver_user:
+#                 return Response(f"User with student_id {receiver_stuID} Not Found!", status=status.HTTP_404_NOT_FOUND)
+#
+#             try:
+#                 sender_contact = Contact.objects.get(user__student_id=sender_stuID)
+#             except Contact.DoesNotExist:
+#                 sender_contact = Contact.objects.create(user=sender_user)
+#                 sender_contact.save()
+#
+#             try:
+#                 receiver_contact = Contact.objects.get(user__student_id=receiver_stuID)
+#                 if receiver_contact:
+#                     return Response("This chatroom has already exists!", status=status.HTTP_302_FOUND)
+#             except Contact.DoesNotExist:
+#                 receiver_contact = Contact.objects.create(user=receiver_user)
+#                 receiver_contact.save()
+#
+#             sender_contact.friends.add(receiver_contact)
+#
+#             try:
+#                 new_chat = Chat.objects.get(title=request.POST['title'])
+#             except Chat.DoesNotExist:
+#                 new_chat = Chat.objects.create(title=request.POST['title'])
+#             new_chat.user.add(sender_contact)
+#             new_chat.user.add(receiver_contact)
+#
+#             return Response(f"Chatroom with id {new_chat.id} has successfully created "
+#                             f"between {sender_user.student_id} & {receiver_user.student_id}!", status=status.HTTP_201_CREATED)
+#         return Response(chat_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# class ChatDetails(APIView):
+#     def get(self, request, stuID):
+#         user = get_user(stuID)
+#         if not user:
+#             return Response(f"User with student_id {stuID} Not Found!", status=status.HTTP_404_NOT_FOUND)
+#
+#         try:
+#             contact = Contact.objects.get(user=user)
+#         except Contact.DoesNotExist:
+#             return Response(f"Contact with student_id {stuID} Not Found!", status=status.HTTP_404_NOT_FOUND)
+#
+#         chats = contact.chats.all()
+#         serializer = ChatSerializer(chats, many=True)
+#         return serializer.data
 
 
 #
@@ -112,20 +122,6 @@ class ChatDetails(APIView):
 #             return Response(f"User with student_id {stuID} Not Found!", status=status.HTTP_404_NOT_FOUND)
 #         user_to_delete.delete()
 #         return Response(f"The user with student_id {stuID} has deleted successfully!", status=status.HTTP_204_NO_CONTENT)
-
-
-class ContactList(APIView):
-    def get(self, request):
-        all_contacts = Contact.objects.all()
-        serializer = ContactSerializer(all_contacts, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = ContactSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # class ContactDetails(APIView):
 #     def get(self, request, stuID):
--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from . import views
+from . import views, api
+
 app_name = 'chat'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:room_name>/', views.room, name='room'),
-
+    # path('chat-room-list/<int:stuID>', api.ChatDetails.as_view()),
+    # path('chat-contact-list/', api.ContactList.as_view()),
+    # path('chat-contacts-list/<int:stuID>', api.ContactDetails.as_view()),
 ]
