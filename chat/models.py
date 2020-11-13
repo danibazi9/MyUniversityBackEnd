42c60f8c45286ccd9f55dd4fd3ad53bc4e8dad38
Danial <danibazi9@gmail.com>
2020-11-13 22:49:22 +0330

    Add the tables for chat Database to models.py

--- chat/models.py
+++ chat/models.py
@@ -1,16 +1,34 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from account.models import Account
 
-User = get_user_model()
+
+# Create your models here.
+class Contact(models.Model):
+    user = models.OneToOneField(Account, related_name='friends', on_delete=models.CASCADE)
+    friends = models.ManyToManyField('self', blank=True)
+
+    def __str__(self):
+        return self.user.username + " " + str(self.user.student_id)
 
 
 class Message(models.Model):
-    author = models.ForeignKey(User, related_name='author_messages', on_delete=models.CASCADE)
-    content = models.TextField()
+    # Chat message created by the user inside a ChatRoom (Foreign key)
+    contact = models.ForeignKey(Contact, related_name='messages', on_delete=models.CASCADE)
+    content = models.TextField(unique=False, blank=False)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.author.username
+        return self.contact.user.username + ": " + self.content
 
-    def last_30_messages():
-        return Message.objects.order_by('-timestamp').all()[:30]
+
+class Chat(models.Model):
+    title = models.CharField(max_length=30)
+    user = models.ManyToManyField(Contact, related_name='chats')
+    messages = models.ManyToManyField(Message, blank=True)
+
+    def last_10_messages(self):
+        return self.messages.objects.order_by('-timestamp').all()[:10]
+
+    def __str__(self):
+        return "ID: " + str(self.id) + " Title: " + self.title
