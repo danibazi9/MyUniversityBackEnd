e437f11701f87e48f1eedc69ffa30f6699b36c60
Danial <danibazi9@gmail.com>
2020-11-14 02:54:29 +0330

    Fix the bug of student_id in account table

--- account/models.py
+++ account/models.py
@@ -63,7 +63,7 @@ class Account(AbstractBaseUser):
     objects = MyAccountManager()
 
     def __str__(self):
-        return self.email + " " + str(self.student_id)
+        return self.username + ", " + str(self.student_id)
 
     # For checking permissions. to keep it simple all admin have ALL permissions
     def has_perm(self, perm, obj=None):
