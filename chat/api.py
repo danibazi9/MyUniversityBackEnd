4a47392b753dbb18b54d841b3f7e6d06f4dacc49
Danial <danibazi9@gmail.com>
2020-11-14 17:16:36 +0330

    Create an API to create a room if doesn't exist & if exist, return room_id of room

--- chat/api.py
+++ chat/api.py
@@ -34,100 +34,28 @@ def chat_properties_view(request):
         serializer = RoomSerializer(rooms, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-#     def post(self, request):
-#         chat_serializer = ChatSerializer(data=request.data)
-#         if chat_serializer.is_valid():
-#             sender_stuID = request.POST['sender_student_id']
-#             receiver_stuID = request.POST['receiver_student_id']
-#
-#             sender_user = get_user(sender_stuID)
-#             if not sender_user:
-#                 return Response(f"User with student_id {sender_stuID} Not Found!", status=status.HTTP_404_NOT_FOUND)
-#
-#             receiver_user = get_user(receiver_stuID)
-#             if not receiver_user:
-#                 return Response(f"User with student_id {receiver_stuID} Not Found!", status=status.HTTP_404_NOT_FOUND)
-#
-#             try:
-#                 sender_contact = Contact.objects.get(user__student_id=sender_stuID)
-#             except Contact.DoesNotExist:
-#                 sender_contact = Contact.objects.create(user=sender_user)
-#                 sender_contact.save()
-#
-#             try:
-#                 receiver_contact = Contact.objects.get(user__student_id=receiver_stuID)
-#                 if receiver_contact:
-#                     return Response("This chatroom has already exists!", status=status.HTTP_302_FOUND)
-#             except Contact.DoesNotExist:
-#                 receiver_contact = Contact.objects.create(user=receiver_user)
-#                 receiver_contact.save()
-#
-#             sender_contact.friends.add(receiver_contact)
-#
-#             try:
-#                 new_chat = Chat.objects.get(title=request.POST['title'])
-#             except Chat.DoesNotExist:
-#                 new_chat = Chat.objects.create(title=request.POST['title'])
-#             new_chat.user.add(sender_contact)
-#             new_chat.user.add(receiver_contact)
-#
-#             return Response(f"Chatroom with id {new_chat.id} has successfully created "
-#                             f"between {sender_user.student_id} & {receiver_user.student_id}!", status=status.HTTP_201_CREATED)
-#         return Response(chat_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#
-# class ChatDetails(APIView):
-#     def get(self, request, stuID):
-#         user = get_user(stuID)
-#         if not user:
-#             return Response(f"User with student_id {stuID} Not Found!", status=status.HTTP_404_NOT_FOUND)
-#
-#         try:
-#             contact = Contact.objects.get(user=user)
-#         except Contact.DoesNotExist:
-#             return Response(f"Contact with student_id {stuID} Not Found!", status=status.HTTP_404_NOT_FOUND)
-#
-#         chats = contact.chats.all()
-#         serializer = ChatSerializer(chats, many=True)
-#         return serializer.data
 
+@api_view(['POST', ])
+def create_room_view(request):
+    try:
+        account = request.user
+    except Account.DoesNotExist:
+        return Response("Authentication user Failed!", status=status.HTTP_404_NOT_FOUND)
 
-#
-#     def put(self, request, stuID):
-#         user_to_edit = get_user(stuID)
-#         if not user_to_edit:
-#             return Response(f"User with student_id {stuID} Not Found!", status=status.HTTP_404_NOT_FOUND)
-#         serializer = UserSerializer(user_to_edit, data=request.data)
-#         if serializer.is_valid():
-#             if 'email' in serializer.validated_data:
-#                 if validate_email(serializer.validated_data.get('email')):
-#                     if serializer.validated_data.get('email').endswith('.iust.ac.ir'):
-#                         serializer.save()
-#                         return Response(serializer.data, status=status.HTTP_201_CREATED)
-#                     else:
-#                         return Response(f"The email '{serializer['email'].value}' isn't the academical university email",
-#                                         status=status.HTTP_406_NOT_ACCEPTABLE)
-#                 else:
-#                     return Response(f"The email '{serializer['email'].value}' doesn't exist",
-#                                     status=status.HTTP_406_NOT_ACCEPTABLE)
-#             else:
-#                 serializer.save()
-#                 return Response(serializer.data, status=status.HTTP_201_CREATED)
-#
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#     def delete(self, request, stuID):
-#         user_to_delete = get_user(stuID)
-#         if not user_to_delete:
-#             return Response(f"User with student_id {stuID} Not Found!", status=status.HTTP_404_NOT_FOUND)
-#         user_to_delete.delete()
-#         return Response(f"The user with student_id {stuID} has deleted successfully!", status=status.HTTP_204_NO_CONTENT)
+    try:
+        sender = Account.objects.get(username=request.POST['sender_username'])
+    except Account.DoesNotExist:
+        return Response(f"The account with username {request.POST['sender_username']} doesn't exist!",
+                        status=status.HTTP_404_NOT_FOUND)
 
-# class ContactDetails(APIView):
-#     def get(self, request, stuID):
-#         user = get_user(stuID)
-#         if not user:
-#             return Response(f"User with student_id {stuID} Not Found!", status=status.HTTP_404_NOT_FOUND)
-#         contact = get_object_or_404(Contact, user=user)
-#         serializer = ChatSerializer(contact.chats().all())
-#         return Response(serializer.data)
+    if request.method == 'POST':
+        room = Room.objects.filter(first_user_id=account, second_user_id=sender) | \
+               Room.objects.filter(first_user_id=sender, second_user_id=account)
+        if room:
+            serializer = RoomSerializer(room, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            room = Room.objects.create(first_user_id=account, second_user_id=sender)
+            room.save()
+            serializer = RoomSerializer(room)
+            return Response(serializer.data, status=status.HTTP_200_OK)
