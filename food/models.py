34925ea4fbae94275bc0ae5eb273b537facb0a3c
Danial <danibazi9@gmail.com>
2020-11-27 07:07:58 +0330

    Add some fields in the tables of Food & Serve

--- food/models.py
+++ food/models.py
@@ -7,12 +7,13 @@ class Food(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=300)
     image = models.ImageField(upload_to='images/', blank=True)
-    cost = models.IntegerField()
+    cost = models.IntegerField(default=0)
 
 
 class Serve(models.Model):
     food_id = models.ForeignKey(Food, on_delete=models.CASCADE)
     seller_id = models.ForeignKey(Account, on_delete=models.CASCADE)
-    time = models.TimeField()
-    date = models.DateTimeField(auto_now_add=True)
-    count = models.IntegerField()
+    start_serve_time = models.TimeField()
+    end_serve_time = models.TimeField()
+    date = models.DateField(auto_now_add=True)
+    count = models.IntegerField(default=0)
