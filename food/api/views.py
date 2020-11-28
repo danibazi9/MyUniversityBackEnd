ec3e082850ffe26e0bde3b621e6a896f6c085aa3
Danial <danibazi9@gmail.com>
2020-11-28 04:35:49 +0330

    Create the API for edit an order that has belongs to different serves (reduce or increase the capacity in the Serve table)

--- food/api/views.py
+++ food/api/views.py
@@ -286,3 +286,99 @@ class AddOrder(APIView):
         return Response("Order food completed successfully!", status=status.HTTP_200_OK)
 
 
+@permission_classes((IsAuthenticated,))
+class EditOrder(APIView):
+    def put(self, arg):
+        try:
+            customer = self.request.user
+        except:
+            return Response(f"Authentication Error! Invalid token", status=status.HTTP_400_BAD_REQUEST)
+
+        request_body = json.loads(self.request.body)
+
+        if 'food_list' not in request_body:
+            return Response("BAD REQUEST, food_list required!", status=status.HTTP_400_BAD_REQUEST)
+
+        food_list = request_body['food_list']
+
+        if len(food_list) == 0:
+            return Response("BAD REQUEST, food_list is empty!", status=status.HTTP_400_BAD_REQUEST)
+
+        order_id = self.request.query_params.get('order_id', None)
+        if order_id is not None:
+            try:
+                order_to_edit = Order.objects.get(order_id=order_id)
+            except Order.DoesNotExist:
+                return Response(f"order_id={order_id}, NOT FOUND", status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response("order_id: None, BAD REQUEST ", status=status.HTTP_400_BAD_REQUEST)
+
+        serveid_count_dict = {}
+        serveid_before_count_dict = {}
+        for food in food_list:
+            if 'serve_id' not in food:
+                return Response("BAD REQUEST, serve_id required in food_list!", status=status.HTTP_400_BAD_REQUEST)
+            if 'count' not in food:
+                return Response("BAD REQUEST, count required in food_list!", status=status.HTTP_400_BAD_REQUEST)
+
+            serve_id = food['serve_id']
+            count = food['count']
+
+            try:
+                serve_to_choose = Serve.objects.get(serve_id=serve_id)
+
+                end_serve_time = serve_to_choose.end_serve_time
+                serve_date = serve_to_choose.date
+                time_of_serve = datetime.datetime(year=serve_date.year, month=serve_date.month,
+                                                  day=serve_date.day, hour=end_serve_time.hour,
+                                                  minute=end_serve_time.minute, second=end_serve_time.second)
+
+                if time_of_serve.timestamp() < datetime.datetime.now().timestamp():
+                    return Response(f"Serve with serve_id {serve_id} is for the past, you can't reserve any food!")
+            except Serve.DoesNotExist:
+                return Response(f"Serve with serve_id {serve_id} NOT FOUND!", status=status.HTTP_404_NOT_FOUND)
+
+            before_count = 0
+            dict_of_found = {}
+            for order_item in order_to_edit.ordered_items.split(" + "):
+                dict_of_found[order_item.split(",")[0]] = False
+                if order_item.split(",")[0] == serve_to_choose.food.name:
+                    start_index = order_item.split(",")[1].find(":")
+                    stop_index = order_item.split(",")[1].find("*")
+                    before_count = int(order_item.split(",")[1][start_index + 1:stop_index].strip())
+                    dict_of_found[order_item.split(",")[0]] = True
+                    break
+
+            for key in dict_of_found:
+                if not dict_of_found[key]:
+                    serve_to_increase_count = Serve.objects.get(food__name=key)
+                    serve_to_increase_count.remaining_count += count
+                    serve_to_increase_count.save()
+
+            if count > serve_to_choose.remaining_count + before_count:
+                return Response(f"No more food! serve_id {serve_id} has only "
+                                f"{serve_to_choose.remaining_count + before_count} food to serve, "
+                                f"that {before_count} of them are reserved by you",
+                                status=status.HTTP_406_NOT_ACCEPTABLE)
+
+            serveid_count_dict[serve_id] = count
+            serveid_before_count_dict[serve_id] = before_count
+
+        ordered_items_list = []
+        total_price = 0
+
+        for key in serveid_count_dict.keys():
+            serve_to_reduce_count = Serve.objects.get(serve_id=key)
+            serve_to_reduce_count.remaining_count += serveid_before_count_dict[key]
+            serve_to_reduce_count.remaining_count -= serveid_count_dict[key]
+            serve_to_reduce_count.save()
+            ordered_items_list.append(f"{serve_to_reduce_count.food.name}, "
+                                      f"Price: {serveid_count_dict[key]} * {serve_to_reduce_count.food.cost}R")
+            total_price += serveid_count_dict[key] * serve_to_reduce_count.food.cost
+
+        order_to_edit.ordered_items = " + ".join(ordered_items_list)
+        order_to_edit.total_price = total_price
+        order_to_edit.save()
+        return Response("Order food has edited successfully!", status=status.HTTP_200_OK)
+
+
