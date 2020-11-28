90ca2bb3364262b7f6afe2a7eb9eb9759c95a2c7
Danial <danibazi9@gmail.com>
2020-11-28 04:23:43 +0330

    Add serializers for user serve & order

--- food/api/serializer.py
+++ food/api/serializer.py
@@ -10,7 +10,36 @@ class FoodSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class ServeSerializer(serializers.ModelSerializer):
+class AdminAllServeSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Serve
+        fields = '__all__'
+
+
+class AdminServeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+
+class UserAllServeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Serve
+        fields = ('start_serve_time', 'end_serve_time', 'remaining_count')
+
+
+class UserServeSerializer(serializers.ModelSerializer):
+    food = FoodSerializer(read_only=True)
+
+    class Meta:
+        model = Serve
+        fields = ('serve_id', 'food', 'remaining_count')
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
         fields = '__all__'
