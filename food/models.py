85286c5f394f2fa320eb6035686b13b8356c5330
Danial <danibazi9@gmail.com>
2020-11-29 01:28:27 +0330

    Set date of serve to be not unique

--- food/models.py
+++ food/models.py
@@ -19,7 +19,7 @@ class Serve(models.Model):
     seller = models.ForeignKey(Account, on_delete=models.CASCADE)
     start_serve_time = models.TimeField()
     end_serve_time = models.TimeField()
-    date = models.DateField(auto_now_add=True, unique=True)
+    date = models.DateField(auto_now_add=True)
     remaining_count = models.IntegerField()
     max_count = models.IntegerField()
 
