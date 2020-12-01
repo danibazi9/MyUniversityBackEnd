a7c5ed7ed3426f74fba3ca130ba29a8ab7d93813
Danial <danibazi9@gmail.com>
2020-12-01 13:20:20 +0330

    Customize django administration for the event tables

--- event/admin.py
+++ event/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from . models import *
 
-# Register your models here.
+
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ['organization_id', 'name', 'head_of_organization']
+    search_fields = ['name', 'head_of_organization']
+    list_filter = ['name', 'head_of_organization']
+
+    class Meta:
+        model = Organization
+
+
+admin.site.register(Organization, OrganizationAdmin)
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['event_id', 'name', 'organizer', 'start_time', 'end_time', 'hold_type']
+    search_fields = ['name', 'organizer', 'description']
+    list_filter = ['organizer', 'hold_type']
+
+    class Meta:
+        model = Event
+
+
+admin.site.register(Event, EventAdmin)
