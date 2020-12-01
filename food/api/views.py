e1cee2a3176a085cdd540379286cc8aac6e09b47
sina_ziaee <sinaZiaee99@gmail.com>
2020-12-02 05:00:59 +1030

    nothing

--- food/api/views.py
+++ food/api/views.py
@@ -276,19 +276,6 @@ class AdminOrdersAll(APIView):
         for x in data:
             x['customer_username'] = Account.objects.get(user_id=x['customer']).username
             x['customer_student_id'] = Account.objects.get(user_id=x['customer']).student_id
-        for x in data:
-            x['items'] = []
-            for item in x['ordered_items'].split(" + "):
-                start_index = item.split(",")[1].find(":")
-                stop_index = item.split(",")[1].find("*")
-                count = int(item.split(",")[1][start_index + 1:stop_index].strip())
-
-                r_index = item.split(",")[1].find("R")
-                price = int(item.split(",")[1][stop_index + 1:r_index].strip())
-
-                item_dict = {'name': item.split(",")[0], 'count': count, 'price': price}
-                x['items'].append(item_dict)
-            del x['ordered_items']
         return Response(data, status=status.HTTP_200_OK)
 
 
