aee694b2efc016cdda24ddc31d523b7f42a5f414
Danial <danibazi9@gmail.com>
2020-12-08 20:11:03 +0330

    Add the table EventAuthorizedOrganizer to grant access for events

--- event/models.py
+++ event/models.py
@@ -5,12 +5,20 @@ from account.models import Account
 class Organization(models.Model):
     organization_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    established_year = models.IntegerField()
+    established_year = models.IntegerField(blank=True)
     head_of_organization = models.ForeignKey(Account, on_delete=models.CASCADE)
     description = models.CharField(max_length=500, blank=True)
 
     def __str__(self):
         return self.name + ",    Head: " + self.head_of_organization.first_name + " " + self.head_of_organization.last_name
+
+
+class EventAuthorizedOrganizer(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
 
 
 class Event(models.Model):
@@ -23,14 +31,16 @@ class Event(models.Model):
     end_time = models.DateTimeField()
 
     HOLD_CHOICES = (
-        ('Presence', 'Presence'),
-        ('Online', 'Online'),
+        ('Presence', 'حضوری'),
+        ('Online', 'آنلاین'),
     )
     hold_type = models.CharField(max_length=10, choices=HOLD_CHOICES)
     location = models.CharField(max_length=255)
     cost = models.IntegerField(default=0)
-    capacity = models.IntegerField(default=0)
+    capacity = models.IntegerField(default=100)
+    remaining_capacity = models.IntegerField(default=100)
     verified = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.name + "    Organized by: " + self.organizer.name
+        return self.name + "    Organized by: " + self.organizer.name + \
+               " Remaining capacity: " + str(self.remaining_capacity)
