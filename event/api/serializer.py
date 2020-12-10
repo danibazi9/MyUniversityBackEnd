285b1c7a7b7df270e15d15a1b5d9fb5bfa3ad368
Danial <danibazi9@gmail.com>
2020-12-10 16:07:28 +0330

    Add serializer for events in serializer.py

--- event/api/serializer.py
+++ event/api/serializer.py
@@ -3,6 +3,10 @@ from ..models import *
 
 
 class EventSerializer(serializers.ModelSerializer):
+    start_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+    end_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+    organizer = serializers.StringRelatedField(read_only=True)
+
     class Meta:
         model = Event
         fields = '__all__'
