dcf34c2085b3185cdf17cd7d979c58d44c2bb0f6
Danial <danibazi9@gmail.com>
2020-12-10 13:04:03 +0330

    Customize Django Administration for CultureDeputy table

--- event/admin.py
+++ event/admin.py
@@ -38,6 +38,30 @@ class EventAuthorizedOrganizerAdmin(admin.ModelAdmin):
 admin.site.register(EventAuthorizedOrganizer, EventAuthorizedOrganizerAdmin)
 
 
+class CultureDeputyAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'faculty_name']
+    search_fields = ['user', 'faculty']
+    list_filter = ['user', 'faculty']
+
+    def first_name(self, obj):
+        result = Account.objects.get(user_id=obj.user.user_id)
+        return result.first_name
+
+    def last_name(self, obj):
+        result = Account.objects.get(user_id=obj.user.user_id)
+        return result.last_name
+
+    def faculty_name(self, obj):
+        result = Faculty.objects.get(id=obj.faculty.id)
+        return result.name
+
+    class Meta:
+        model = CultureDeputy
+
+
+admin.site.register(CultureDeputy, CultureDeputyAdmin)
+
+
 class EventAdmin(admin.ModelAdmin):
     list_display = ['event_id', 'name', 'organizer', 'start_time', 'end_time', 'verified']
     search_fields = ['name', 'organizer', 'description']
