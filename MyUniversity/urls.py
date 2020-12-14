38cc4eefebd7d4746622b20a55ca0c4359eaf7a2
Danial <danibazi9@gmail.com>
2020-12-15 02:47:24 +0330

    Remove the User table from MyUniversity app (because we have replaced it with Account table before)

--- MyUniversity/urls.py
+++ MyUniversity/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from . import api
 import chat.api
 
 app_name = 'MyUniversity'
 urlpatterns = [
-    path('api/users-list/', api.UsersList.as_view()),
-    path('api/users-list/<int:stuID>', api.UsersDetails.as_view()),
-    path('api/send-email/<int:stuID>', api.SendEmail.as_view()),
     path('api/chat/', include('chat.urls')),
     path('api/account/', include('account.api.urls', 'account_api')),
     path('api/room-list/', chat.api.chat_properties_view, name='chat'),
