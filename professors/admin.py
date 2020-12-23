8dd895ce145e3ebb58efcd9a27d238cc02183b06
Danial <danibazi9@gmail.com>
2020-12-23 22:45:55 +0330

    Customize the Django Administration for the ResearchAxis table

--- professors/admin.py
+++ professors/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 from . models import *
 
 
+class ResearchAxisAdmin(admin.ModelAdmin):
+    list_display = ['researchaxis_id', 'subject']
+    search_fields = ['subject']
+
+    class Meta:
+        model = Professor
+
+
+admin.site.register(ResearchAxis, ResearchAxisAdmin)
+
+
 class ProfessorAdmin(admin.ModelAdmin):
     list_display = ['professor_id', 'first_name', 'last_name', 'faculty', 'academic_rank', 'email']
     search_fields = ['first_name', 'last_name', 'faculty', 'academic_rank', 'email', 'research_axes']
