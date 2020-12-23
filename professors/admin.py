e9039d4a96bbfbcce93ee379d4923a6f0a8a3936
Danial <danibazi9@gmail.com>
2020-12-23 22:35:46 +0330

    Customize the Django Administration for the Professor table

--- professors/admin.py
+++ professors/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . models import *
 
-# Register your models here.
+
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = ['professor_id', 'first_name', 'last_name', 'faculty', 'academic_rank', 'email']
+    search_fields = ['first_name', 'last_name', 'faculty', 'academic_rank', 'email', 'research_axes']
+    list_filter = ['last_name', 'faculty', 'academic_rank']
+
+    class Meta:
+        model = Professor
+
+
+admin.site.register(Professor, ProfessorAdmin)
