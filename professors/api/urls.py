4c9ca480d62380450293edf51175e0a2e2b31af8
Danial <danibazi9@gmail.com>
2020-12-30 14:36:41 +0330

    Add the urls of APIs of professors, times, faculties, search_axes

--- professors/api/urls.py
+++ professors/api/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from event.api import views
+from professors.api import views
 
 urlpatterns = [
+    path('admin/', views.AdminProfessor.as_view()),
+    path('user/all/', views.get_all_professors),
+    path('user/', views.UserGetProfessor.as_view()),
+    path('times/', views.get_all_times),
+    path('faculty/', views.get_all_faculties),
+    path('research-axes/', views.get_research_axes),
 ]
