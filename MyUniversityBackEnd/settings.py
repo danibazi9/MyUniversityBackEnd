7e9e1019b8afe32e91c683f1249282ca6fbefa41
Danial <danibazi9@gmail.com>
2021-02-03 01:48:02 +0330

    Nothing

--- MyUniversityBackEnd/settings.py
+++ MyUniversityBackEnd/settings.py
@@ -104,7 +104,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('*', 6379)],
+            "hosts": [('127.0.0.1', 6379)],
         },
     },
 }
